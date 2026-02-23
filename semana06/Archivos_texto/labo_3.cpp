3e3276c5fb609682dfb3cdf663d11fbc631b0a09
VHugo <Victor06@gmail.com>
2026-02-23 10:52:55 -0500

    ya estan todas las funciones pedidas (falta consola dinamica)

--- semana06/Archivos_texto/labo_3.cpp
+++ semana06/Archivos_texto/labo_3.cpp
@@ -7,10 +7,10 @@ using namespace std;
 const int N=4;
 
 struct producto { // 56 bytes ejemplo
-    int id;                  // Identificador único
-    char nombre[30];         // Nombre del producto
-    double precio;           // Precio unitario
-    int stock;               // Cantidad disponible
+    int id;                  // Identificador único 4 bytes
+    char nombre[30];         // Nombre del producto 
+    double precio;           // Precio unitario 8 bytes
+    int stock;               // Cantidad disponible 4 bytes
     bool activo;             // true = activo, false = eliminado lógicamente
 };
 
@@ -59,6 +59,7 @@ void mostrarProductos(const char* nombreArchivo){
     p_leer.open(nombreArchivo, ios::binary); 
     // inicializamos el objeto con nombreArchivo en modo binario
 
+
     struct producto prueba; // creamos un tipo de dato donde almacenar los tipos de datos escritos
     while(p_leer.read((char*)&prueba, sizeof(struct producto))){
         cout << "ID: " << prueba.id<< endl; 
@@ -72,7 +73,9 @@ void mostrarProductos(const char* nombreArchivo){
         }
 
         // probemos la funcion tellg();
-        cout << "Posicion: "<<p_leer.tellg() << " bytes" << endl;
+        int posicion = p_leer.tellg(); // dime la posicion siguiente a la actual
+        posicion = posicion - sizeof(struct producto);
+        cout << "Posicion: "<< posicion << " bytes" << endl;
 
         cout << "----------------\n";
     }
@@ -94,8 +97,12 @@ int buscarProductoPorID(const char* nombreArchivo, int idBuscado){
     while(archivo.read((char*)&prueba, sizeof(struct producto))){  
         // esto recorre el archivo (tome en cuenta que el archivo esta lleno de structs del mismo
         // tipo)
+
+        // el struct prueba pasa de struct en struct en todo  el archivo
         if(prueba.id == idBuscado){
-            return archivo.tellg();
+            int posicion = archivo.tellg();
+            posicion = posicion - sizeof(struct producto);
+            return posicion;
         }
     }
 
@@ -103,17 +110,137 @@ int buscarProductoPorID(const char* nombreArchivo, int idBuscado){
     return -1;
 }
 
+void modificarPrecio(const char* nombreArchivo, int id, double nuevoPrecio){
+    fstream archivo; // creo objeto para escribir y leer
+
+    // ahora que lo pienso, porque al entrar con ios::out el archivo no se reescribe?
+    // osea si el archivo existe, solo lo abre?
+    archivo.open(nombreArchivo, ios::binary | ios::in | ios::out ); 
+    // inicio archivo en modo binario con lectura y escritura
+
+    // como mi archivo esta lleno de structs del mismo tipo, puedo recorrerlo de la siguiente forma
+    struct producto prueba;
+    int posicion; 
+//    archivo.tellp(-sizeof(struct producto), ios::cur);
+    while(archivo.read((char*)&prueba, sizeof(struct producto))){
+        // este while pasa de struct en struct igualando prueba a cada struct
+        /* prueba == producto[0]        posicion -> 0
+        prueba == producto[1]           posicion -> 1
+        prueba == producto[2]           posicion -> 2
+        ...
+        */
+        
+        if(prueba.id == id){ 
+            posicion = archivo.tellg(); 
+            /* como read pone a ambos punteros( de fstream) en la misma posicion, da un poco igual
+            si uso tellp() o tellg() */ 
+            posicion -= sizeof(struct producto); 
+            break;
+        }
+    }
+
+    // despues de esta funcion, prueba apunta a 102 y archivo apunta a 103 
+
+
+
+    // prueba apunta al producto buscado por su id, y posicion(en bytes) apunta a su posicion xd
+    prueba.precio = nuevoPrecio; // modificamos prueba
+
+    // ya tengo la posicion, ahora a modificarlo
+    archivo.seekp(posicion); // dirijo el puntero escribir a la posicion del cambio
+    archivo.write((char*)&prueba, sizeof(struct producto));
+    // que pasa si ingreso una struct diferente  
+
+    archivo.close(); // desvinculamos, ahorita explico lo que hace lo anterior
+
+}
+
+void eliminarProducto(const char* nombreArchivo, int id){
+    // voy a ir al id especifico y desactivare su producto (false)
+
+    int posicion = buscarProductoPorID(nombreArchivo, id); // posicion en bytes
+    // no olvidar que tengo mi archivo entero de struct iguales
+
+    fstream archivo; // creo objeto de lectura y escritura
+
+    archivo.open(nombreArchivo, ios::binary | ios::in | ios::out); 
+    // inicio el objeto en modo binario , modo lectura y modo escritura en ese orden
+
+    archivo.seekp(posicion); // puntero escritura apunta a direccion
+    archivo.seekg(posicion);  // puntero lectura apunta a direccion
+
+    struct producto prueba;
+
+    archivo.read((char*)&prueba, sizeof(struct producto)); 
+    /* Se supone y esto lee la estructura (struct producto) de la posicion .seekg() y la 
+    "copia" en prueba (que tambien es struct producto) 
+    Tambien */
+
+    prueba.activo = false; // hago el cambio y ahora reemplazo
+
+    // no olvides que despues de usar read o write, el puntero leer y escribir, aumentan su 
+    // posicion en una estructura
+
+    /* Es necesario entonces volver a apuntar los puntero en la posicion */
+    archivo.seekp(posicion); // puntero escritura apunta a direccion
+    archivo.seekg(posicion);  // puntero lectura apunta a direccion
+
+    archivo.write((char*)&prueba, sizeof(struct producto)); 
+    /* Esto reemplaza la estructura de la posicion .seekp() por la variable prueba de un tamaño
+    en bytes de sizeof(struct producto) */
+
+    archivo.close(); // cerrando el archivo 
+
+}
+
+double calcularValorInventario(const char* nombreArchivo){
+    // solo suma los activos
+
+    double total=0;
+    fstream archivo; // creo objeto de lectura y escritura
+
+    archivo.open(nombreArchivo, ios::binary | ios::in | ios:: out); 
+    // me olvide inicializar, modo binario, lectura y escritura
+
+    // el puntero leer y escribir estan en 0
+    struct producto prueba;
+    while(archivo.read((char*)&prueba, sizeof(struct producto)) ){
+        // ahora prueba tiene la info de la struct que apunta el puntero
+        if(prueba.activo){
+            total += double(prueba.stock) * prueba.precio;
+        }
+    }
+
+    archivo.close(); // cerando ciclos xd
+    return total;
+}
+
+
 int main(){
     const char *nombre = {"inventario.dat"};
 
-    registrarProducto(nombre);
+    registrarProducto(nombre); 
+    /* como tu eres el primer binary | out , entonces tu funcion es crear y escribir el archivo
+    los demas binary | out se encargan de escribir no mas */
+
+    //mostrarProductos(nombre);
+
+    int posicion = buscarProductoPorID(nombre, 102);
+    cout <<  "Posicion prueba: " << posicion << " bytes" << endl;
+    // vamos a editar en dicha posicion (en bytes)
+
+    double precioNuevo=34;
+
+    // modificar y busqueda por id (no por posicion)
+    modificarPrecio(nombre, 102, precioNuevo);
+
+    eliminarProducto(nombre, 102);
+
+    double total = calcularValorInventario(nombre);
+    cout << "Precio total: " << total << endl;
 
     mostrarProductos(nombre);
 
-    int posicion = buscarProductoPorID(nombre, 102);
-    cout << posicion << endl;
 
-
-    
     return 0;
 }
