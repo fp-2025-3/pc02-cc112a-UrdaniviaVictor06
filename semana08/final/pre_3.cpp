66b9d4e8666eeff4bba236b2f9bc8bf1e33f5aca
VHugo <Victor06@gmail.com>
2026-03-06 21:07:33 -0500

    aun me falta resolver el ejercicio3

---
+++ semana08/final/pre_3.cpp
@@ -0,0 +1,252 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+
+using namespace std;
+
+
+struct proyecto{ // 64 bytes
+    int id;
+    char titulo[40];
+    double presupuesto;
+    int duracionMeses;
+};
+
+
+//_____________________
+
+
+void crear_archivo(string nombre_archivo){
+    fstream archivo; // objeto lectura y escritura
+
+    archivo.open(nombre_archivo, ios::binary | ios::in); 
+    // si el archivo se abre, significa que ya esta creado
+
+    if(!archivo){
+        // si el archivo no abre, es porque no esta creado
+        cout << "\nopcion 2\n";
+        archivo.close(); // desvinculamos
+
+        archivo.open(nombre_archivo, ios::binary | ios::out); 
+        // creamos el archivo en modo bimario
+        archivo.close(); // desvinculamos nuevamente;
+    } else {
+        // significa que ya esta creado
+        archivo.close(); // desvinculamos y ya
+        cout << "\nopcion 1\n";
+    }
+
+    
+}
+
+struct proyecto *agregar_proyectos(string nombre_archivo, int &n_final){
+    int n=-1;
+    while(n<=0){
+        cout << "\nIngrese la cantidad de proyectos que desea ingresar: ";
+        cin >>n;
+    }
+
+    n_final =n; // actualizamos la cantidad
+
+    struct proyecto *lista = new struct proyecto[n]; // crea una lista de n proyectos
+    for(int i=0; i< n;i++){
+        lista[i].presupuesto = -1;
+        while(lista[i].presupuesto <=0){
+            cout << "\nIngrese el presupuesto de su proyecto "<< i+1 << ": ";
+            cin >>lista[i].presupuesto;
+        } 
+        //aseguramos que el dinero sea positivo
+
+        int cantidad=i+1; // cantidad = cantidad temporal
+        bool bandera1 = true;
+        lista[i].id =-1;
+        while(lista[i].id <= 0 || bandera1){
+            bandera1 =false; // con esta linea, puedes salir del while si no se repite el id
+            cout << "\nIngrese el id de su proyecto (no repetir): ";
+            cin >>lista[i].id;
+
+            for(int j=0; j <cantidad; j++){
+                if(lista[j].id == lista[i].id){
+                    if(i==j) continue; // no se cuenta el mismo
+
+                    // si entras aqui se supone y se repite
+                    cout << "\nse repite el id \n";
+                    bandera1 = true;
+                }
+            }
+
+        }
+        // con esto aseguro que sea positivo y que no se repita el id
+
+        cin.ignore();
+        cout << " \nIngrese el titulo de su proyecto: ";
+        cin.getline(lista[i].titulo, 40);
+    
+        lista[i].duracionMeses =-1;
+        while(lista[i].duracionMeses <=0){
+            cout << "ingrese la duracion en meses de su proyecto: ";
+            cin >> lista[i].duracionMeses;
+        }
+
+    }
+
+    // llenamos el archivo en binario y descendiente con presupuesto
+
+    fstream archivo; // objeto lectur ay escritura
+
+    archivo.open(nombre_archivo, ios::binary | ios::out | ios::in);
+    // modo binario lectura y escritura
+
+    for(int i=0; i< n; i++){
+        int indice_max=i; // supongamos que es el mayor
+
+        for(int j=i; j<n; j++){
+            if(lista[j].presupuesto > lista[indice_max].presupuesto){
+                // significa que hay uno mayor
+                indice_max = j;
+            }
+        }
+
+        //ya tenemos al mayor, ahora swap
+        swap(lista[i].presupuesto, lista[indice_max].presupuesto);
+        swap(lista[i].id, lista[indice_max].id);
+        swap(lista[i].duracionMeses, lista[indice_max].duracionMeses);
+
+        // swap con char
+        char temp[40];
+        strcpy(temp, lista[i].titulo);
+        strcpy(lista[i].titulo, lista[indice_max].titulo);
+        strcpy(lista[indice_max].titulo, temp);
+
+    }
+
+    // ya tenemos la lista ordenada
+    for(int i=0; i< n; i++){
+        // la lista a ingresar es 
+        // lista[lista_ind[i]] 
+        archivo.write((char*)&lista[i], sizeof(struct proyecto));
+        // copia la lista en bytes y mueve el puntero 
+    }
+
+
+    archivo.close();
+    return lista;
+}
+
+void insertarProyecto(string nombre_archivo, int &n, struct proyecto *lista){
+    struct proyecto prueba;
+
+    cout << "\n Vamos a ingresar un proyecto nuevo\n";
+
+    prueba.id = -1;
+    bool bandera =true;
+    while(prueba.id <=0 || bandera){
+        bandera = false; // ya puedes salir
+
+        cout << "Ingrese un id para su proyecto (sin repetir): ";
+        cin >> prueba.id;
+
+        for(int i=0; i<n; i++){
+            if(lista[i].id == prueba.id){
+                // significa que se repite el id
+                cout << "se repite el id\n";
+                bandera = true;
+            }
+        }
+    }
+
+    prueba.presupuesto = -1;
+    while(prueba.presupuesto <=0){
+        cout << "\nIngrese el presupuesto de su proyecto: ";
+        cin >> prueba.presupuesto;
+    }
+
+    cin.ignore();
+    cout << "\nIngrese el titulo de su proyecto : ";
+    cin.getline(prueba.titulo, 40); 
+
+    prueba.duracionMeses = -1;
+    while(prueba.duracionMeses <=0){
+        cout << "Ingrese la duracion en meses de su proyecto: ";
+        cin >>prueba.duracionMeses;
+    }
+
+    fstream archivo; // objeto de lectura y escritura
+
+    archivo.open(nombre_archivo, ios::binary | ios::out | ios::in);
+    // modo bianrio
+
+    struct proyecto copia1;
+    
+    while(archivo.read((char*)&copia1, sizeof(struct proyecto))){
+        // copia 1 tiene toda la info
+
+        
+        if(copia1.presupuesto < prueba.presupuesto ){
+            // 
+            int posicion = archivo.tellg();
+            posicion = posicion - sizeof(struct proyecto);
+            
+            archivo.seekp(posicion);
+            archivo.seekg(posicion);
+
+            // escribimos la prueba, luego la copia1 se pasa a prueba, sigue su rumbo
+            archivo.write((char*)&prueba, sizeof(struct proyecto)); 
+            // copiamos en binario
+
+            // copiamos copia en prueba
+            prueba.presupuesto = copia1.presupuesto;
+            prueba.id = copia1.id;
+            prueba.duracionMeses = copia1.duracionMeses;
+
+            char temp[40];
+            strcpy(prueba.titulo, copia1.titulo);
+            // listo ya cambiamos
+
+        }
+    }
+    
+    archivo.close(); 
+
+}
+
+void mostrar_proyectos(string nombre_archivo){
+    fstream archivo; // objeto lectura y escritura
+
+    archivo.open(nombre_archivo, ios::binary | ios::out | ios::in); 
+    // modo binario lectura y escritura
+
+    struct proyecto prueba;
+    while(archivo.read((char*)&prueba, sizeof(struct proyecto))){
+        // prueba tiene toda la info
+
+        cout << "\nID: "<<prueba.id << " | Titulo: "<<prueba.titulo<< endl;
+        cout << "Presupuesto: "<<prueba.presupuesto << " | Duracion (meses) : ";
+        cout << prueba.duracionMeses << endl;
+        cout << "------------------------------------\n";
+
+    }
+
+    archivo.close(); 
+}
+
+int main(){
+
+    string nombre_archivo = "proyectos.dat";
+
+    crear_archivo(nombre_archivo);
+
+    int n;
+    struct proyecto *lista = agregar_proyectos(nombre_archivo, n);
+
+    mostrar_proyectos(nombre_archivo);
+
+    insertarProyecto(nombre_archivo,n,lista);
+
+    mostrar_proyectos(nombre_archivo);
+
+
+    delete[] lista;
+    return 0;
+}
